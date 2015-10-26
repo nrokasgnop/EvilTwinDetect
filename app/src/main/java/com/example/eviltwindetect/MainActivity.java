4a2389a181256f7bda7dc20736a0d6a9ef2f3233
apple <nrokasgnop@gmail.com>
2015-10-26 21:08:22 +0700

    can connect wifi no password

--- app/src/main/java/com/example/eviltwindetect/MainActivity.java
+++ app/src/main/java/com/example/eviltwindetect/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.eviltwindetect;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.WifiConfiguration;
 import android.support.v7.app.AppCompatActivity;
 import android.content.BroadcastReceiver;
 
@@ -11,10 +14,14 @@ import android.net.wifi.WifiManager;
 import android.net.wifi.ScanResult;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -25,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
     String wifis[];
     WifiScanReceiver wifiReciever;
 
+
+    String networkSSID = "";
+    String networkPass = "";
+    static long itemSelect = 0;
+
+
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         wifiReciever = new WifiScanReceiver();
         wifi.startScan();
+
     }
 
     protected void onPause() {
@@ -67,9 +83,71 @@ public class MainActivity extends AppCompatActivity {
             wifis = new String[wifiScanList.size()];
 
             for(int i = 0; i < wifiScanList.size(); i++){
-                wifis[i] = ((wifiScanList.get(i)).toString());
+                //wifis[i] = ((wifiScanList.get(i)).toString());
+                wifis[i] = "SSID: " + ((wifiScanList.get(i).SSID).toString()) + "\n" +
+                        "BSSID: "+ ((wifiScanList.get(i).BSSID).toString()) + "\n" +
+                        "Capabilities: "+ ((wifiScanList.get(i).capabilities).toString()) + "\n" +
+                        "Level: " + ((wifiScanList.get(i).level)) + "\n" +
+                        "Frequency: " + ((wifiScanList.get(i).frequency));
             }
-            list.setAdapter(new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,wifis));
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                    getApplicationContext(),android.R.layout.simple_list_item_1,wifis);
+            list.setAdapter(adapter);
+            showDialog();
         }
     }
+
+    private void showDialog() {
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                itemSelect = id;
+                builder.setTitle("Wi-Fi status");
+                builder.setMessage("Do you want to connect?");
+                builder.setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String item = Long.toString(itemSelect);
+                        connecting(itemSelect);
+                        //Toast.makeText(getApplicationContext(), item + "Connected", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("Disconnect", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Not Connect", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
+    private void connecting(long itemSelect) {
+        List<ScanResult> wifiScanList = wifi.getScanResults();
+        WifiConfiguration conf = new WifiConfiguration();
+        networkSSID = ((wifiScanList.get((int) itemSelect).SSID).toString());
+        conf.SSID = "\"" + networkSSID + "\"";
+
+        conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+        //conf.preSharedKey = "\""+ networkPass +"\"";
+        wifi.addNetwork(conf);
+
+        List<WifiConfiguration> list1 = wifi.getConfiguredNetworks();
+
+        for (WifiConfiguration i : list1) {
+            if (i.SSID != null && i.SSID.equals("\"" + networkSSID + "\"")) {
+                Toast.makeText(getApplicationContext(), networkSSID + " Connected", Toast.LENGTH_SHORT).show();
+                wifi.disconnect();
+                wifi.enableNetwork(i.networkId, true);
+                wifi.reconnect();
+                break;
+            }
+        }
+    }
+
 }
